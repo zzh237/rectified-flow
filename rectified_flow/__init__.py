7bef32f50884ce999c767c62a7f02de8ec233004
lqiang67 <lqiang67@gmail.com>
2024-11-17 23:10:25 -0600

    Update __init__.py

--- rectified_flow/__init__.py
+++ rectified_flow/__init__.py
@@ -1,4 +1,5 @@
 from .rectified_flow import RectifiedFlow, AffineInterp, AffineInterpSolver, CouplingDataset
-from .samplers import SDESampler, EulerSampler, OverShootingSampler, CurvedSampler
+from .samplers import SDESampler, EulerSampler, OverShootingSampler, CurvedEulerSampler, NoiseRefreshSampler
 
-__all__ = ["CouplingDataset", "RectifiedFlow", "AffineInterp", "AffineInterpSolver",  "OverShootingSampler", "EulerSampler", "SDESampler"]
+__all__ = ["CouplingDataset", "RectifiedFlow", "AffineInterp", "AffineInterpSolver",  
+           "OverShootingSampler", "EulerSampler", "SDESampler", "CurvedEulerSampler", "NoiseRefreshSampler"]
