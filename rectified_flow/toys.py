36cde0f60c9b8e22e1d151a82b0e088c3ca1460d
lqiang67 <lqiang67@gmail.com>
2024-11-17 22:29:26 -0600

    Update toys.py

--- rectified_flow/toys.py
+++ rectified_flow/toys.py
@@ -4,6 +4,14 @@ import torch.nn as nn
 import torch.distributions as dist
 from collections import namedtuple
 
+from types import MethodType
+
+def mixture_sample_with_labels(self, sample_shape=torch.Size()):
+    labels = self.mixture_distribution.sample(sample_shape)
+    all_samples = self.component_distribution.sample(sample_shape)
+    x_samples = all_samples[torch.arange(len(labels)), labels]
+    return x_samples, labels
+
 # create toy gaussian mixture models
 def create_circular_gmm(n_components, radius, dim=2, std = 1.0, device=None):
     if device is None: device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -13,6 +21,7 @@ def create_circular_gmm(n_components, radius, dim=2, std = 1.0, device=None):
     weights = torch.ones(n_components).to(device)
     weights /= weights.sum()
     gmm = dist.MixtureSameFamily(dist.Categorical(weights), dist.Independent(dist.Normal(means, stds), 1))
+    gmm.sample_with_labels = MethodType(mixture_sample_with_labels, gmm)
     return gmm
 
 def create_two_point_gmm(x=10.0, y=10.0, std=1.0, device=None):
@@ -23,6 +32,7 @@ def create_two_point_gmm(x=10.0, y=10.0, std=1.0, device=None):
   stds = torch.ones(n_components, n_features).to(device) * std
   weights = torch.ones(n_components).to(device); weights /= weights.sum()
   gmm = dist.MixtureSameFamily(dist.Categorical(weights), dist.Independent(dist.Normal(means, stds), 1))
+  gmm.sample_with_labels = MethodType(mixture_sample_with_labels, gmm)
   return gmm
 
 # generic MLP model
@@ -44,7 +54,7 @@ class MLP(nn.Module):
         return x
 
 # a model for velocity field v(x,t)
-class MLPWithTimeInput(nn.Module):
+class MLPVelocity(nn.Module):
   def __init__(self, dim, hidden_sizes=[128, 128, 128], output_dim = None):
     super().__init__()
     if output_dim is None:
@@ -57,3 +67,37 @@ class MLPWithTimeInput(nn.Module):
     xt = torch.cat((x, t), dim=1)
     return self.mlp(xt)
 
+
+class MLPVelocityConditioned(nn.Module):
+    def __init__(self, dim, hidden_sizes=[128, 128, 128], output_dim=None, label_dim=1):
+        super().__init__()
+        if output_dim is None:
+            output_dim = dim
+        self.label_dim = label_dim
+        self.mlp = MLP([dim + 1 + label_dim, *hidden_sizes, output_dim])
+
+    def forward(self, x, t, labels=None):
+        t = t.squeeze()  # Ensure t is in the correct shape
+        t = t.view(t.shape[0], -1)
+        x = x.view(x.shape[0], -1)
+
+        # If labels are provided, process them
+        if labels is not None:
+            # If labels are discrete, convert them to one-hot encoding
+            #labels = torch.nn.functional.one_hot(labels, num_classes=self.label_dim).float() if self.label_dim else labels
+            labels = labels.float()
+            labels = labels.view(labels.shape[0], -1)
+            xt = torch.cat((x, t, labels), dim=1)
+        else:
+            xt = torch.cat((x, t), dim=1)
+
+        return self.mlp(xt)
+
+
+if __name__ == '__main__':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    gmm = create_two_point_gmm(x=10.0, y=10.0, std=1.0, device=device)
+    x, labels = gmm.sample_with_labels([3])
+    t = torch.rand(x.shape[0]).to(device)
+    model = MLPVelocityConditioned(2).to(device)
+    y = model(x, t, labels)
