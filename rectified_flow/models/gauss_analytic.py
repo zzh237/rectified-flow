Fix gradient detach

rectified_flow/models/gauss_analytic.py
@@ -20,7 +20,7 @@ class AnalyticGaussianVelocity(nn.Module):
         self.dataset_norm = self.dataset.norm(dim=1).pow(2)
         self.interp = interp
 
-    @torch.inference_mode()
+    @torch.no_grad()
     def forward(self, x_t, t):
         """
         x_t shape: (Batch, D)

rectified_flow/models/kernel_method.py
@@ -5,33 +5,33 @@ from rectified_flow.flow_components.interpolation_solver import AffineInterp
 class NadarayaWatson(nn.Module):
     def __init__(
         self, 
-        pi_0: torch.Tensor,
-        pi_1: torch.Tensor,
+        pi_0_sample: torch.Tensor,
+        pi_1_sample: torch.Tensor,
         sample_size: int,
         interp: AffineInterp = AffineInterp("straight"),
         bandwidth: float = 0.4,
         use_dot_x_t: bool = True,
     ):
         super().__init__()
-        self.datashape = pi_0.shape[1:]
-        self.dataset_size = pi_0.shape[0]
-        self.pi_0 = pi_0.detach().clone().reshape(pi_0.shape[0], -1).requires_grad_(False)
-        self.pi_1 = pi_1.detach().clone().reshape(pi_1.shape[0], -1).requires_grad_(False)
-        assert self.pi_0.shape == self.pi_1.shape, "pi_0 and pi_1 must have the same shape"
+        self.datashape = pi_0_sample.shape[1:]
+        self.dataset_size = pi_0_sample.shape[0]
+        self.pi_0_sample = pi_0_sample.detach().clone().reshape(pi_0_sample.shape[0], -1).requires_grad_(False)
+        self.pi_1_sample = pi_1_sample.detach().clone().reshape(pi_1_sample.shape[0], -1).requires_grad_(False)
+        assert self.pi_0_sample.shape == self.pi_1_sample.shape, "pi_0_sample and pi_1_sample must have the same shape"
         self.sample_size = sample_size
         self.interp = interp
         self.bandwidth = bandwidth
         self.use_dot_x_t = use_dot_x_t
         
-    @torch.inference_mode()
+    @torch.no_grad()
     def forward(self, z_t, t):
         """
         z_t: (batch_size, dim)
         t: (batch_size, )
         """
         idx = torch.randperm(self.dataset_size)[:self.sample_size]
-        x_0 = self.pi_0[idx, :].detach().clone() # (sample_size, dim)
-        x_1 = self.pi_1[idx, :].detach().clone() # (sample_size, dim)
+        x_0 = self.pi_0_sample[idx, :].detach().clone() # (sample_size, dim)
+        x_1 = self.pi_1_sample[idx, :].detach().clone() # (sample_size, dim)
         x_1 = x_1[None, :, :].expand(z_t.shape[0], -1, -1)     # (batch_size, sample_size, dim)
         x_0 = x_0[None, :, :].expand(z_t.shape[0], -1, -1)     # (batch_size, sample_size, dim)
         z_t = z_t[:, None, :].expand(-1, self.sample_size, -1) # (batch_size, sample_size, dim)